db9201ed0742fe8ccc39c3a8d15e9cff3d31cdd9
lumiere-eclair <100339268+lumiere-eclair@users.noreply.github.com>
2022-04-08 01:08:31 +0800

    :sparkles: new project

---
+++ go.mod
@@ -0,0 +1,8 @@
+module chaty
+
+go 1.13
+
+require (
+	github.com/gorilla/websocket v1.5.0
+	github.com/satori/go.uuid v1.2.0
+)
---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import "encoding/json"
+
+func (manager *ClientManager) start() {
+    for {
+        select {
+        case conn := <-manager.register: {
+            manager.clients[conn] = true
+            jsonMessage, _ := json.Marshal(&Message{
+                Content: "/A new socket has connected.",
+            })
+            manager.send(jsonMessage, conn)
+        }
+        case conn := <-manager.unregister: {
+            if _, ok := manager.clients[conn]; ok {
+                close(conn.send)
+                delete(manager.clients, conn)
+                jsonMessage, _ := json.Marshal(&Message{
+                    Content: "/A socket has disconnected.",
+                })
+                manager.send(jsonMessage, conn)
+            }
+        }
+        case message := <-manager.broadcast: {
+            for conn := range manager.clients {
+                select {
+                case conn.send <- message:
+                default: {
+                    close(conn.send)
+                    delete(manager.clients, conn)
+                }
+                }
+            }
+        }
+        }
+    }
+}
